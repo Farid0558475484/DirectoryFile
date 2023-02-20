1d65007a6236179402fa41b3343992786fd29645
Farid0558475484 <faridmh@code.edu.az>
2023-02-20 10:22:54 +0400

    streamreader

--- aqza/Program.cs
+++ aqza/Program.cs
@@ -9,8 +9,8 @@ namespace qwerty
         {
 
             Directory.CreateDirectory(@"/Users/xamyak/Desktop/Test");
-            Directory.CreateDirectory(@"/Users/xamyak/Desktop/Test/salam.txt");
-            Directory.CreateDirectory(@"/Users/xamyak/Desktop/Test/dewre.txt");
+            //Directory.CreateDirectory(@"/Users/xamyak/Desktop/Test/salam.txt");
+            //Directory.CreateDirectory(@"/Users/xamyak/Desktop/Test/dewre.txt");
             //Directory.Delete(@"/Users/xamyak/Desktop/Test", true);
             //if (Directory.Exists(@"/Users/xamyak/Desktop/Test/salam.txt"))
             //{
@@ -18,14 +18,33 @@ namespace qwerty
 
             //}
 
+            //string[] arr = Directory.GetFiles(@"/Users/xamyak/Desktop/Test");
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Console.WriteLine("salam");
+
+            //File.Create(@"/Users/xamyak/Desktop/Test/salam.txt");
+
             string[] arr = Directory.GetFiles(@"/Users/xamyak/Desktop/Test");
             foreach (var item in arr)
             {
                 Console.WriteLine(item);
             }
 
-            Console.WriteLine("salam");
+            StreamWriter sw = new StreamWriter(@"/Users/xamyak/Desktop/Test/salam.txt");
+            sw.WriteLine("qwertyujhdgdfs");
+            sw.Close();
+
+
+            StreamReader sw1 = new StreamReader(@"/Users/xamyak/Desktop/Test/salam.txt");
+            string result = sw1.ReadToEnd();
+            Console.WriteLine(result);
         }
+
+
     }
 
 }
