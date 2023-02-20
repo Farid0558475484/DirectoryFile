66560932463228507a4be27d6cc589f009ed479c
Farid0558475484 <faridmh@code.edu.az>
2023-02-20 09:27:12 +0400

    directory file create delete

---
+++ aqza/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace qwerty
+{
+
+    class EmptyClass
+    {
+        static void Main(string[] args)
+        {
+
+            Directory.CreateDirectory(@"/Users/xamyak/Desktop/Test");
+            Directory.CreateDirectory(@"/Users/xamyak/Desktop/Test/salam.txt");
+            Directory.CreateDirectory(@"/Users/xamyak/Desktop/Test/dewre.txt");
+            //Directory.Delete(@"/Users/xamyak/Desktop/Test", true);
+            //if (Directory.Exists(@"/Users/xamyak/Desktop/Test/salam.txt"))
+            //{
+            //    Directory.Delete(@"/Users/xamyak/Desktop/Test",true);
+
+            //}
+
+            string[] arr = Directory.GetFiles(@"/Users/xamyak/Desktop/Test");
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("salam");
+        }
+    }
+
+}
